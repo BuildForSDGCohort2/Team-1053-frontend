d4f9ede42a89587175c1e9463652ad316952d74e
Bonifase Orwa <orwabonifase@gmail.com>
2020-10-06 23:26:42 +0300

    set up tests

--- src/app/services/user/app.service.spec.ts
+++ src/app/services/user/app.service.spec.ts
@@ -1,15 +1,3 @@
-import { AppService } from './app.service';
-import { User } from '../../models/app.model';
-import { Observable } from 'rxjs';
 
 describe('User Authentication', () => {
-    let authService: AppService;
-    let mockHttp;
-    beforeEach(() => {
-        mockHttp = jasmine.createSpyObj('mockHttp', ['post', 'put'])
-        authService = new AppService(mockHttp);
-    });
-    describe('Register User', () => {
-        it('Register new user', () => { });
-    });
 });
